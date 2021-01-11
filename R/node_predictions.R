3bc74154c59c39e3bf5e3d8ba349203249e3ab72
Angelo D'Ambrosio <bakaburg1@gmail.com>
2021-01-11 18:24:45 +0100

    docs: fix math rendering in html docs

--- R/node_predictions.R
+++ R/node_predictions.R
@@ -2,21 +2,22 @@
 #'
 #' This function provides maximum likelihood point estimates of the outcome for
 #' each node in the tree (albeit it can be used with any vector of values). It
-#' optionally also computes the confidence intervals around those estimates, using a
-#' normal approximation \eqn{mean ± Z_{\alpha}\times SE} for continuous values
-#' and `stats::binom.test()` exact binomial intervals for discrete (i.e. character,
+#' optionally also computes the confidence intervals around those estimates,
+#' using a normal approximation \ifelse{html}{\out{<i>mean ± Z<sub>&alpha;</sub>
+#' SE</i>}}{\eqn{mean ± Z_{\alpha} SE}} for continuous values and
+#' `stats::binom.test()` exact binomial intervals for discrete (i.e.: character,
 #' factor, logical) ones.
 #'
-#' @details The function is passed as the `est_fun` argument of
-#'   `tidy_tree()`, but works also as stand-alone. This is a default estimation
-#'   method Estimates can be extremely noisy in decision trees, especially in
-#'   small terminal nodes; therefore more robust solutions (e.g. Bayesian
-#'   regularized intervals) are a better choice.
+#' @details The function is passed as the `est_fun` argument of `tidy_tree()`,
+#'   but works also as stand-alone. This is a default estimation method
+#'   Estimates can be extremely noisy in decision trees, especially in small
+#'   terminal nodes; therefore more robust solutions (e.g. Bayesian regularized
+#'   intervals) are a better choice.
 #'
 #' @param values Values of the outcome in a tree node.
 #' @param add_interval Whether to compute confidence intervals.
-#' @param interval_level Confidence level. Must be strictly greater than 0 and less
-#'   than 1. Defaults to 0.95, which corresponds to a 95 percent confidence
+#' @param interval_level Confidence level. Must be strictly greater than 0 and
+#'   less than 1. Defaults to 0.95, which corresponds to a 95 percent confidence
 #'   interval.
 #'
 #' @return A  tibble with the estimates and the interval boundaries, with
